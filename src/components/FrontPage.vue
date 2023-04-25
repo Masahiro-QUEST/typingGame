5bcfcbe46f12043f1e0dafa8e892494c3f4d2b8d
masahiro.yamada <xwrokmailmasa@gmail.com>
2023-04-25 09:38:54 +0900

    秒数の処理を追加

--- src/components/FrontPage.vue
+++ src/components/FrontPage.vue
@@ -20,6 +20,12 @@
       >
         Clear!
       </div>
+      <div
+        v-if="current_question_counts == question_count"
+        class="py-16 text-4xl clear text-blue-500"
+      >
+        {{ elapsedTime }}秒
+      </div>
       <button
         v-if="current_question_counts == question_count"
         class="py-4 px-8 bg-blue-500 text-white rounded-full hover:bg-blue-700 focus:outline-none mx-auto"
@@ -29,10 +35,11 @@
       </button>
       <div class="typeFormWrapper mb-20">
         <input
+          v-if="current_question_counts != question_count"
           id="typeForm"
           v-model="typeBox"
           type="text"
-          class="typeForm text-white font-bold text-center bg-transparent border-b-2 border-white w-64 focus:outline-none"
+          class="typeForm text-white font-bold text-lg text-center bg-transparent border-b-2 border-white w-64 focus:outline-none"
           name=""
         />
       </div>
@@ -56,6 +63,8 @@ export default {
       typeBox: "",
       current_question_counts: 0,
       question_count: 0,
+      startTime: null,
+      elapsedTime: null,
     };
   },
   computed: {
@@ -70,16 +79,21 @@ export default {
   methods: {
     gameStart: function () {
       this.startFlg = true;
+      this.startTime = new Date();
       this.$nextTick(function () {
         document.getElementById("typeForm").focus();
       });
     },
     restartGame: function () {
       this.current_question_counts = 0;
+      this.startTime = new Date();
       this.questions = ["apple", "banana", "chocolate", "donut", "espresso"];
       this.current_question = this.questions[0];
       this.startFlg = true;
       this.typeBox = "";
+      this.$nextTick(function () {
+        document.getElementById("typeForm").focus();
+      });
     },
   },
   mounted: function () {
@@ -93,6 +107,13 @@ export default {
         this.questions.splice(0, 1);
         this.typeBox = "";
         this.current_question_counts += 1;
+        // クリア条件が満たされた場合
+        if (this.current_question_counts == this.question_count) {
+          // ゲーム終了時刻を取得し、経過時間を計算
+          let endTime = new Date();
+          this.elapsedTime = (endTime - this.startTime) / 1000;
+          // elapsedTimeを表示または利用する処理を追加
+        }
       }
     },
   },
@@ -108,8 +129,8 @@ export default {
   transition: all 0.3s ease;
 }
 .gaugeWrapper {
-  border: 1px solid white;
+  border: 1px solid #3e3e3e;
   height: 12px;
-  width: 64rem;
+  width: 32rem;
 }
 </style>
