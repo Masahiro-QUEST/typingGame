c70f0cabca817eeae95ede8f69ea191f886746a8
masahiro.yamada <xwrokmailmasa@gmail.com>
2023-05-19 17:06:30 +0900

    プレイアゲインの時もスペースキーで反応するよう機能追加

--- src/components/game/PlayAgainButtonComponent.vue
+++ src/components/game/PlayAgainButtonComponent.vue
@@ -4,6 +4,27 @@
     style="margin: 20px"
     @click="$emit('restart-game')"
   >
-    もういちどニャ
+    もういちどスペースニャ？
   </button>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      onkeyup: null,
+    };
+  },
+  mounted() {
+    this.onkeyup = (event) => {
+      if (event.key === " ") {
+        this.$emit("restart-game");
+      }
+    };
+    window.addEventListener("keyup", this.onkeyup);
+  },
+  beforeUnmount() {
+    window.removeEventListener("keyup", this.onkeyup);
+  },
+};
+</script>
